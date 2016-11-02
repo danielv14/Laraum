8d3c010f53d7bf7c892af743056c7ae9c2c0843b
Daniel <daniel.vernberg@gmail.com>
2016-11-02 18:05:26 +0100

    link to create new post from nav

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -35,6 +35,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{route('post.create')}}">Create post</a></li>
                             <li>
                                 <a href="{{ url('/logout') }}"
                                     onclick="event.preventDefault();
