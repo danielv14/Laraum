75d6b2963abc6d3805bb1ba6618462e0f77601d4
Daniel <daniel.vernberg@gmail.com>
2016-11-02 18:05:31 +0100

    remove h1

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
   <div class="container">
-    <h1>Laraum medium clone</h1>
-
     @foreach ($posts as $post)
       <h2><a href="{{route('page.show', $post->slug)}}">{{$post->title}}</a></h2>
       <p>created by {{$post->user->name}}</p>
