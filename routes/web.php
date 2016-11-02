4d76613de9aa03de0d4abc0f8b7a44369e18e462
Daniel <daniel.vernberg@gmail.com>
2016-11-02 18:28:12 +0100

    cleaned up routes file from generated comments

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'PagesController@index');
 Route::get('/post/{slug}', 'PagesController@show')->name('page.show');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
 
 /*
 * Group up routes for profile CRUD and such
@@ -27,6 +14,5 @@ Route::group(['prefix' => 'profile'], function() {
   Route::resource('post', 'PostController', [
     'except' => ['index']
   ]);
-
 
 });
