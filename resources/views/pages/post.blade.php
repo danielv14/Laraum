8cda6c45c2e16f8f8442995876b836ca3f4b0a2b
Daniel <daniel.vernberg@gmail.com>
2016-11-07 18:43:25 +0100

    wrap logic in unless-statement

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -7,21 +7,24 @@
       <div class="column is-3">
           <h2 class="title">By <a href="{{route('pages.user', $post->user->id)}}">{{$post->user->name}}</a></h2>
           <h4 class="subtitle">{{ $post->created_at->diffForHumans() }}</h4>
-          @if (Auth::user()->id == $post->user_id)
-            <div class="control is-grouped">
-              <p class="control">
-                <a class="button" href="{{route('post.edit', $post->id)}}">Edit post</a>
-              </p>
-              <p class="control">
-                {!! Form::open([
-                  'method' => 'DELETE',
-                  'route' => ['post.destroy', $post->id]
-                ]) !!}
-                  {!! Form::submit('Delete', ['class' => 'button is-danger is-outlined']) !!}
-                {!! Form::close() !!}
-              </p>
-            </div>
-          @endif
+          @unless (Auth::guest())
+            @if (Auth::user()->id == $post->user_id)
+              <div class="control is-grouped">
+                <p class="control">
+                  <a class="button" href="{{route('post.edit', $post->id)}}">Edit post</a>
+                </p>
+                <p class="control">
+                  {!! Form::open([
+                    'method' => 'DELETE',
+                    'route' => ['post.destroy', $post->id]
+                  ]) !!}
+                    {!! Form::submit('Delete', ['class' => 'button is-danger is-outlined']) !!}
+                  {!! Form::close() !!}
+                </p>
+              </div>
+            @endif
+          @endunless
+    
       </div>
 
       <div class="column is-9">
