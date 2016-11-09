4fc0257c1764321de6f7de206189359d1c69405e
Daniel <daniel.vernberg@gmail.com>
2016-11-09 11:16:15 +0100

    visual spacing to better seperate columns

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
       <div class="columns">
+
         <div class="column is-9">
           @foreach ($posts as $post)
             <div class="section">
@@ -21,22 +22,19 @@
                 <br>
                 <a class="button" href="{{route('pages.post', $post->slug)}}">Read more</a>
             </div>
-
-
           @endforeach
         </div>
+        
         <div class="column is-3 is-hidden-mobile">
           <div class="content box">
             <h2 class="title">Popular posts</h2>
             @foreach ($popular as $post)
               <h4 class="subtitle"><a href="{{route('pages.post', $post->slug)}}">{{ $post->title }}</a></h4>
-
             @endforeach
           </div>
         </div>
 
       </div>
-
     </div>
 
 
