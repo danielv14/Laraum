b4e652ac6cc8ae5b17509f5e6c271854b59be842
Daniel <daniel.vernberg@gmail.com>
2016-11-19 23:41:25 +0100

    wrap bookmark in p-tag to force new line

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -7,7 +7,9 @@
       <div class="column is-2">
           <h2 class="title">By <a href="{{route('pages.user', $post->user->id)}}">{{$post->user->name}}</a></h2>
           <h4 class="subtitle">{{ $post->created_at->diffForHumans() }}</h4>
-          @include('partials._bookmark')
+          <p>
+            @include('partials._bookmark')
+          </p>
           @unless (Auth::guest())
             @if (Auth::user()->id == $post->user_id)
               <a class="button" href="{{route('post.edit', $post->id)}}">Edit post</a>
