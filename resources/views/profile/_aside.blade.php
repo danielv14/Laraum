3a184b012713bc550350cf1b1c926f7327ed3b4e
Daniel <daniel.vernberg@gmail.com>
2017-01-15 19:04:55 +0100

    slightly darker bg color. better layout of profile aside partial

--- resources/views/profile/_aside.blade.php
+++ resources/views/profile/_aside.blade.php
@@ -1,13 +1,27 @@
 <aside class="menu box">
   <p class="menu-label">My posts</p>
   <ul class="menu-list">
-    <li><a class="{{ set_active_route('profile.index') }}" href="{{route('profile.index')}}">{{$posts->count()}} Published</a></li>
-    <li><a class="{{ set_active_route('profile.drafts') }}" href="{{route('profile.drafts')}}">{{ $drafts->count() }} {{ str_plural('Draft', $drafts->count()) }}</a></li>
-    <li><a class="{{ set_active_route('profile.bookmarks') }}" href="{{route('profile.bookmarks')}}">{{ $bookmarks->count() }} {{ str_plural('Bookmark', $bookmarks->count()) }}</a></li>
-
-    <li><a href="{{ route('post.create') }}">Create post</a></li>
-
+    <li>
+      <a class="{{ set_active_route('profile.index') }}"
+         href="{{route('profile.index')}}">
+        {{$posts->count()}} Published
+      </a>
+    </li>
+    <li>
+      <a class="{{ set_active_route('profile.drafts') }}"
+         href="{{route('profile.drafts')}}">
+        {{ $drafts->count() }} {{ str_plural('Draft', $drafts->count()) }}
+      </a>
+    </li>
+    <li>
+      <a class="{{ set_active_route('profile.bookmarks') }}"
+         href="{{route('profile.bookmarks')}}">
+        {{ $bookmarks->count() }} {{ str_plural('Bookmark', $bookmarks->count()) }}
+      </a>
+    </li>
   </ul>
+  <br>
+  <a class="button is-primary is-outlined" href="{{ route('post.create') }}">Create post</a>
   <p class="menu-label">My profile</p>
   <ul class="menu-list">
     <li><a href="{{ url('/logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log out</a></li>
