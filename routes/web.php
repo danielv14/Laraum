bf0f66e5578c6f7274ae777384bdfaa86ae594c8
Daniel <daniel.vernberg@gmail.com>
2017-01-17 16:37:08 +0100

    comments and better structure of web routes

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,34 @@
 <?php
 
+// Index route
 Route::get('/', 'PagesController@index');
+// View all posts
 Route::get('/post/{slug}', 'PagesController@post')->name('pages.post');
-
+// View a specific user
 Route::get('/user/{id}', 'PagesController@user')->name('pages.user');
-
+// Authentication routes
 Auth::routes();
+// Bookmarking routes
+Route::put('/bookmark', 'BookmarkController@store');
+Route::delete('/bookmark/{id}', 'BookmarkController@destroy');
+// Like routes
+Route::put('/like/{id}', 'LikesController@like');
+Route::put('/unlike/{id}', 'LikesController@unlike');
 
 /*
 * Group up routes for profile CRUD and such
 */
 Route::group(['prefix' => 'profile'], function() {
-
+  // Current users profile
   Route::get('/', 'ProfileController@index')->name('profile.index');
+  // Current users drafts
   Route::get('/drafts', 'ProfileController@drafts')->name('profile.drafts');
+  // Current users bookmarks
   Route::get('/bookmarks', 'ProfileController@bookmarks')->name('profile.bookmarks');
 
-  // CRUD Post
+  // CRUD Resource for Current users post
   Route::resource('post', 'PostController', [
     'except' => ['index']
   ]);
 
 });
-
-// Bookmarking routes
-Route::put('/bookmark', 'BookmarkController@store');
-Route::delete('/bookmark/{id}', 'BookmarkController@destroy');
-
-// Like routes
-Route::put('/like/{id}', 'LikesController@like');
-Route::put('/unlike/{id}', 'LikesController@unlike');
